dd3619c0daf2d6d7737aa5f10086511b32885d9a
sergey <user357x@yandex.ru>
2017-05-13 22:18:32 +0700

    fg

--- tools.js
+++ tools.js
@@ -119,7 +119,7 @@ const getLastPosition = (i) => function* () {
     let otsing = otsings[i];
     let region = regions;
 
-    const position = yield db.orgs.getLastPosition('OU', 'all');
+    const position = yield db.orgs.getLastPosition(otsing, region instanceof Array ? 'all' : region.name);
 
     if(!position) {
 
