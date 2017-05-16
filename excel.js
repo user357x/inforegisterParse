99aee7464dbcb0a6bddf2f985d6dcaed5d1e7a30
sergey <user357x@yandex.ru>
2017-05-16 12:11:53 +0700

    xdbvdfg

--- excel.js
+++ excel.js
@@ -7,12 +7,6 @@ const xl = require('excel4node');
  
 const wb = new xl.Workbook();
 
-//const wsAll = wb.addWorksheet('all');
-//const wsHarjumaa = wb.addWorksheet('harjumaa');
-
-//let wsAll = wb.addWorksheet('all');
-//let wsHarjumaa = wb.addWorksheet('harjumaa');
-
 const style = wb.createStyle({
     font: {
         color: '#FF0800',
@@ -20,28 +14,31 @@ const style = wb.createStyle({
     }
 });
 
+let all = 'all';
+let harjumaa = 'harjumaa';
+
 db.task(function* () {
 
 	let data, j = 2;
 
-	let wsAll = wb.addWorksheet('all');
+	let ws = wb.addWorksheet(harjumaa);
 
-    wsAll.cell(1,1).string('name').style(style);
-	wsAll.cell(1,2).string('phone').style(style);
-	wsAll.cell(1,3).string('email').style(style);
-	wsAll.cell(1,4).string('address').style(style);
-	wsAll.cell(1,5).string('sphere').style(style);
+    ws.cell(1,1).string('name').style(style);
+	ws.cell(1,2).string('phone').style(style);
+	ws.cell(1,3).string('email').style(style);
+	ws.cell(1,4).string('address').style(style);
+	ws.cell(1,5).string('sphere').style(style);
 
-	data = yield db.orgs.getByRegion('OU', 'all');
+	data = yield db.orgs.getByRegion('TU', harjumaa);
 
 	data.forEach((org, i) => {
 		j = i + 2;
 		//console.log(j);
-		wsAll.cell(j, 1).string(org.body.name ? org.body.name : '');
-		wsAll.cell(j, 2).string(org.body.phone ? org.body.phone : '');
-		wsAll.cell(j, 3).string(org.body.email ? org.body.email : '');
-		wsAll.cell(j, 4).string(org.body.address ? org.body.address : '');
-		wsAll.cell(j, 5).string(org.body.sphere ? org.body.sphere : '');
+		ws.cell(j, 1).string(org.body.name ? org.body.name : '');
+		ws.cell(j, 2).string(org.body.phone ? org.body.phone : '');
+		ws.cell(j, 3).string(org.body.email ? org.body.email : '');
+		ws.cell(j, 4).string(org.body.address ? org.body.address : '');
+		ws.cell(j, 5).string(org.body.sphere ? org.body.sphere : '');
 	});
 	
 
@@ -77,6 +74,6 @@ db.task(function* () {
 	});
 	*/
 
-	wb.write('./xls/OU/all.xlsx');
+	wb.write('./xls/TU/harjumaa.xlsx');
 
 }).catch(console.error);
