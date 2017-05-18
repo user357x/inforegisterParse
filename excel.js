e2f8a4088645b0b053cdf2c9c564cf756637953a
sergey <user357x@yandex.ru>
2017-05-18 21:22:34 +0700

    excel

--- excel.js
+++ excel.js
@@ -4,76 +4,64 @@ global.config = require(`${__dirname}/./config`);
 global.db = require(`${__dirname}/./postgres`)(config.postgres);
 
 const xl = require('excel4node');
- 
-const wb = new xl.Workbook();
 
-const style = wb.createStyle({
-    font: {
-        color: '#FF0800',
-        size: 12
-    }
-});
-
-let all = 'all';
-let harjumaa = 'harjumaa';
+let otsing = 'OU';
+let region = 'harjumaa';
 
 db.task(function* () {
 
-	let data, j = 2;
+	let wb, ws, emails, style, count = 0, j = 2, fileCount = 1;
 
-	let ws = wb.addWorksheet(all);
-
-    ws.cell(1,1).string('name').style(style);
-	ws.cell(1,2).string('phone').style(style);
-	ws.cell(1,3).string('email').style(style);
-	ws.cell(1,4).string('address').style(style);
-	ws.cell(1,5).string('sphere').style(style);
-
-	data = yield db.orgs.getByRegion('TU', all);
-
+	const data = yield db.orgs.getByRegion(otsing, region);
+	
 	data.forEach((org, i) => {
-		j = i + 2;
-		//console.log(j);
-		ws.cell(j, 1).string(org.body.name ? org.body.name : '');
-		ws.cell(j, 2).string(org.body.phone ? org.body.phone : '');
-		ws.cell(j, 3).string(org.body.email ? org.body.email : '');
-		ws.cell(j, 4).string(org.body.address ? org.body.address : '');
-		ws.cell(j, 5).string(org.body.sphere ? org.body.sphere : '');
+		
+		//console.log(org.body.email);
+		//console.log(typeof org.body.email.split);
+
+		emails = org.body.email ? org.body.email.split(', ').filter(email => email.length > 0) : [];
+
+		emails.forEach((email, e) => {
+
+			if(count === 0 || count % 50000 === 0) {
+
+				wb = new xl.Workbook();
+
+				style = wb.createStyle({
+				    font: {
+				        color: '#FF0800',
+				        size: 12
+				    }
+				});
+
+				ws = wb.addWorksheet("orgs");
+
+			    ws.cell(1,1).string('name').style(style);
+				ws.cell(1,2).string('phone').style(style);
+				ws.cell(1,3).string('email').style(style);
+				ws.cell(1,4).string('address').style(style);
+				ws.cell(1,5).string('sphere').style(style);
+				
+			}
+
+			j++;
+			ws.cell(j, 1).string(org.body.name ? org.body.name : '');
+			ws.cell(j, 2).string(org.body.phone ? org.body.phone : '');
+			ws.cell(j, 3).string(email);
+			ws.cell(j, 4).string(org.body.address ? org.body.address : '');
+			ws.cell(j, 5).string(org.body.sphere ? org.body.sphere : '');
+
+			if(count % 50000 === 0 || (i + 1 === data.length && e + 1 === emails.length)) {
+
+				wb.write(`./xls/${otsing}/${region}/${fileCount}.xlsx`);
+				fileCount++;
+
+			}
+
+			count++;
+
+		});
+
 	});
 	
-
-	/*
-	let wsHarjumaa = wb.addWorksheet('harjumaa');
-
-	wsHarjumaa.cell(1,1).string('name').style(style);
-	wsHarjumaa.cell(1,2).string('phone').style(style);
-	wsHarjumaa.cell(1,3).string('email').style(style);
-	wsHarjumaa.cell(1,4).string('address').style(style);
-	wsHarjumaa.cell(1,5).string('sphere').style(style);
-
-	//data = yield db.orgs.getByRegion('OU', 'harjumaa');
-	data = yield db.any(
- 			`select * 
- 			from public.orgs 
- 			where file = $1 and 
- 			region = $2 
- 			order by id 
- 			offset $3 
- 			limit $4`, 
- 			['OU', 'all', 0, 57130]
-		)
-
-	data.forEach((org, i) => {
-		j = i + 2;
-		//console.log(j);
-		wsHarjumaa.cell(j, 1).string(org.body.name ? org.body.name : '');
-		wsHarjumaa.cell(j, 2).string(org.body.phone ? org.body.phone : '');
-		wsHarjumaa.cell(j, 3).string(org.body.email ? org.body.email : '');
-		wsHarjumaa.cell(j, 4).string(org.body.address ? org.body.address : '');
-		wsHarjumaa.cell(j, 5).string(org.body.sphere ? org.body.sphere : '');
-	});
-	*/
-
-	wb.write('./xls/TU/all.xlsx');
-
 }).catch(console.error);
